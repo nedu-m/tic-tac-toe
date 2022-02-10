d4c0b02978a49fb4fa0de58d094cc90ac179ae4b
nedumd <davidsonchinedum@gmail.com>
2022-02-10 17:09:30 +0100

    Added title and footer

--- src/App.js
+++ src/App.js
@@ -5,17 +5,43 @@ import "papercss/dist/paper.min.css";
 
 function App() {
   return (
-    <Main>
+    <>
+      <Title>Tic-Tac-Toe Game</Title>
+      <Main>
       <TicTacToe />
-    </Main>
+      </Main>
+      <Footer>
+        <FooterInner>
+          Built by{" "}
+          <a href="https://nedumdev.netlify.app/">Nedu'm</a>
+        </FooterInner>
+      </Footer>
+    </>
   );
 }
+
+const Title = styled.h3`
+  display: flex;
+  justify-content: center;
+  height: 0vh;
+`;
 
 const Main = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 70vh;
   `;
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  flex: 0 0 auto;
+`;
+
+const FooterInner = styled.h5`
+  padding: 16px 0;
+`;
 
 export default App;
