ac138ed95abcc72afb4ccdf5ba4f2abccdefa6a2
nedu'm <davidsonchinedum@gmail.com>
2022-03-04 09:14:44 +0100

    added styles

--- src/styles.js
+++ src/styles.js
@@ -3,5 +3,5 @@ export const border = `
   border-bottom-right-radius: 225px 15px;
   border-top-left-radius: 255px 15px;
   border-top-right-radius: 15px 225px;
-border: 2px solid #41403e;
+  border: 2px solid #41403e;
 `;
